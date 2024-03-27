d97f7b97a85af3c544b6e7e78b823744b5306e4a
unknown <vincewarpie@gmail.com>
2024-03-27 11:51:36 +0800

    Models.py please check if good to go

---
+++ db/models.py
@@ -0,0 +1,15 @@
+import uuid
+from sqlalchemy import Column, String
+from database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(String, primary_key=True, default=str(uuid.uuid4()))
+    firstname = Column(String)
+    middlename = Column(String)
+    lastname = Column(String)
+    course = Column(String)  # Store courses as a serialized string
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
